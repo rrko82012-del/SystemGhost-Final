5cc6490f2686e96f94b9600939cdb25d165c368d
rrko82012-del <rrko82012@gmail.com>
2026-03-18 16:02:35 +0200

    Create Typography.kt

---
+++ app/src/main/java/com/example/systemghost/ui/theme/Typography.kt
@@ -0,0 +1,8 @@
+package com.example.systemghost.ui.theme
+
+import androidx.compose.material3.Typography
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    // يمكنك إضافة تخصيصات الخطوط هنا مستقبلاً
+)
