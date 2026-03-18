5e560489b4a98872cafed46041d8a6085e9b8b21
rrko82012-del <rrko82012@gmail.com>
2026-03-18 16:01:09 +0200

    Create Color.kt

---
+++ app/src/main/java/com/example/systemghost/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.systemghost.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CyberBackground = Color(0xFF0D0D12) // أسود داكن جداً
+val CyberSurface = Color(0xFF1A1A24)   // رمادي داكن للبطاقات
+val NeonGreen = Color(0xFF00FF41)      // أخضر نيون ساطع
+val CyberSilver = Color(0xFFC0C0C0)    // فضي للنصوص
+val DangerRed = Color(0xFFFF003C)      // أحمر داكن لوضع إيقاف الحماية
